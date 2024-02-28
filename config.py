07b521b6ae91b0c39faa294605f02d7ca510aa05
Justin-do-it-Cai <caiyutong2003@gmail.com>
2024-02-28 18:41:46 +0800

    annotation

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ app_id = "wx8dc6bc0aab45a0dd"
 app_secret = "76771670a3520027adace5246d99b248"
 # 模板消息id
 # 每日消息
-template_id1 = "oGXCpNE-YGAy7uRdb2TQ5m7suhALgBJPuYGnExiaI3I"
+template_id1 = "ix7MU4ut7mDYe7CoRnl_I-Gm734fO12X7UYdh_2gAPQ"
 # 课程消息,上课提醒
 template_id2 = "loe1yHWr************************************"
 # 晚安心语
